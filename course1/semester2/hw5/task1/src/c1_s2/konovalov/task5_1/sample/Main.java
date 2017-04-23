0881dc8af987f76e2f132fb2e4bd6c77ba6b330e
Петр Коновалов < >
2017-04-23 14:44:39 +0300

    created user calculator interface and functionality

---
+++ course1/semester2/hw5/task1/src/c1_s2/konovalov/task5_1/sample/Main.java
@@ -0,0 +1,24 @@
+package c1_s2.konovalov.task5_1.sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Main Window");
+        primaryStage.setScene(new Scene(root, 430, 150));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
