068b55a4222916a994988c3bb432d08e03002ce8
Петр Коновалов < >
2017-05-07 17:49:47 +0300

    made javadoc for Executor

--- course1/semester2/hw6/task1/src/c1_s2/konovalov/task6_1/Executor.java
+++ course1/semester2/hw6/task1/src/c1_s2/konovalov/task6_1/Executor.java
@@ -1,7 +1,15 @@
 package c1_s2.konovalov.task6_1;
 
-public class Executor {
-    public static int executeOperation(char operation, int firstOperand, int secondOperand) {
+/** выполняет арифмитические операции */
+class Executor {
+    /**
+     * выполняте арифмитическую операцию
+     * @param operation арифмитическая операция '+', '-', '*' или '/'
+     * @param firstOperand первый операнд
+     * @param secondOperand второй операнд
+     * @return возвращает результат арифмитической операции
+     */
+    static int executeOperation(char operation, int firstOperand, int secondOperand) {
         switch (operation) {
             case '+':
                 return firstOperand + secondOperand;
